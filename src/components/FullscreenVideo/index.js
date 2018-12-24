311fddad26abb6cbf1033e021a89b48f5b274ec6
Olivier Bossel <olivier.bossel@gmail.com>
2018-12-24 15:25:13 +0100

    clean component

--- src/components/FullscreenVideo/index.js
+++ src/components/FullscreenVideo/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 class FullscreenVideo extends React.PureComponent {
   render() {
-    const { children, ...others } = this.props
+    const { children, src, ...others } = this.props
     return (
       <div>
-        {typeof this.props.src === 'string' && (
-          <video className="fullscreen-video__video" {...others} />
+        {typeof src === 'string' && (
+          <video className="fullscreen-video__video" src={src} {...others} />
         )}
         {children}
       </div>
